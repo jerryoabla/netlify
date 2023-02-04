e918745beec3921c0a34828c6a7aa178e56ed6ea
Jerry Albao <jerry_albao@yahoo.com>
2023-02-03 23:52:38 -0500

    changes

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Deploying react app to netlify.
+          Breaking changes.
         </p>
         
       </header>
